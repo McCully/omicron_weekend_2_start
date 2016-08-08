20b015833f4fef859b8ca6eef5e7979dc161d7f3
Mat McCully <mccully1894@gmail.com>
2016-08-07 20:24:16 -0500

    inital commit

--- server/app.js
+++ server/app.js
@@ -1,13 +1,17 @@
 var express = require("express");
 var app = express();
 var path = require("path");
-var data = require("./public/data/omicron");
+// var data = require("./public/data/omicron");
+
+app.get("/data", function(req,res,next){
+    res.sendFile(path.join(__dirname, "./public/data/omicron.json"));
+});
 
 app.set("port", (process.env.PORT || 5000));
 
-app.get("/data", function(req,res){
-    res.send(data);
-});
+// app.get("/data", function(req,res){
+    // res.send(data);
+// });
 
 app.get("/*", function(req,res){
     var file = req.params[0] || "views/index.html";
--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -1,9 +1,67 @@
 $(document).ready(function(){
-    $.ajax({
-      type: "GET",
-      url: "/data",
-      success: function(data){
-        console.log(data);
-      }
-    });
+  loadData();
+
+  $('#forward').on('click', goFor);
+  $('#previous').on('click' , goBac);
 });
+var primates = []
+var currentPrimate = 0;
+
+function loadData(){
+  $.ajax({
+    type: "GET",
+    url: "/data",
+    success: function(data){
+      console.log(data);
+      var primates = data.omicron;
+      numPrimates();
+      showPrimate(primates, currentPrimate);
+      $('.primateButton').on("click" , primateNum);
+      console.log(primates);
+    },
+
+    error: function() {
+      console.log("ERROR, Link to server has been lost");
+    }
+  });
+}
+
+function numPrimates(){
+  for(var i = 0; i < primates.length; i++){
+    var displayNum = i + 1;
+    $('.numberButt').append('<button class = "primateNumButt">' + displayNum + '</button>');
+  }
+}
+
+function showPrimate(omicronArr , index) {
+  var primates = omicronArr[index];
+  var primerInfo = '<h3>' + primates.name + '</h3><br><p>' + "<a href='https://github.com/'" + primates.git_username + ">https://github.com/" + primates.git_username + "</a><br>" + primates.shoutout + "</p>";
+console.log(primates)
+  if(index == currentPrimate){
+    $('.primateNumButt').removeClass('current');
+    $('#' + currentPrimate).addClass('current');
+  }
+}
+
+function goFor() {
+  if(currentPrimate >= primates.length - 1){
+    currentPrimate = 0;
+  } else {
+    currentPrimate += 1;
+  }
+
+  showPrimate(primates , currentPrimate);
+}
+
+function goBac(){
+  if(currentPrimate == 0) {
+    currentPrimate = primates.length;
+  }
+  currentPrimate -= 1;
+  showPrimate(primates , currentPrimate);
+}
+
+function primateNum() {
+  intPrimeNum = parseInt(this.id);
+  showPrimate(primates , currentPrimate)
+}
